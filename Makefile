9c1ca1b7f907d9e66648baca7719d321dde8988a
Liam Fruzyna <liamfruzyna@gmail.com>
2019-08-11 19:44:35 -0500

    Figured a makefile was a bit better

---
+++ Makefile
@@ -0,0 +1,5 @@
+install:
+	pip3 install --user matplotlib pandas tabulate openpyxl
+
+server:
+	python3 server.py
--- setup.sh
+++
@@ -1,3 +0,0 @@
-# Installs pip dependencies
-#!/bin/bash
-pip3 install --user matplotlib pandas tabulate openpyxl
